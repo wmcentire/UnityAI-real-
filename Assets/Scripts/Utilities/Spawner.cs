332a2f2c7c68080125635eae4c2c82e00682533a
wmcentire <wmcentire@student.neumont.edu>
2023-01-12 10:18:52 -0700

    Added multiple agents

--- Assets/Scripts/Utilities/Spawner.cs
+++ Assets/Scripts/Utilities/Spawner.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public Agent agent;
+    public Agent[] agents;
     public LayerMask layerMask;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
             {
-                Instantiate(agent, hitInfo.point, Quaternion.identity);
+                if(Input.GetKey(KeyCode.Alpha1)) Instantiate(agents[0], hitInfo.point, Quaternion.identity);
+                if(Input.GetKey(KeyCode.Alpha2)) Instantiate(agents[1], hitInfo.point, Quaternion.identity);
             }
         }
     }
