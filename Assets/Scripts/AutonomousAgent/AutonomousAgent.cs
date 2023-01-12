038046389a49a0e77384c4c96ab32e5e3c033173
wmcentire <wmcentire@student.neumont.edu>
2023-01-12 10:12:46 -0700

    Added spawner stuff and wander stuff

--- Assets/Scripts/AutonomousAgent/AutonomousAgent.cs
+++ Assets/Scripts/AutonomousAgent/AutonomousAgent.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class AutonomousAgent : Agent
 {
+    public float wanderDistance = 1;
+    [Range(3,9)]public float wanderRadius = 3;
+    public float wanderDisplacement = 5;
+
+    public float wanderAngle { get; set; } = 0;
+
+
     // Update is called once per frame
     void Update()
     {
         var gameObjects = perception.GetGameObjects();
-        foreach (var gameObject in gameObjects)
+        //foreach (var gameObject in gameObjects)
+        //{
+        //    Debug.DrawLine(transform.position,gameObject.transform.position);
+        //}
+        //if(gameObjects.Length > 0)
+        //{
+        //    movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * 1);
+        //}
+
+        if (movement.acceleration.sqrMagnitude <= movement.maxForce * 0.1f)
         {
-            Debug.DrawLine(transform.position,gameObject.transform.position);
+            movement.ApplyForce(Steering.Wander(this));
         }
-        if(gameObjects.Length > 0)
-        {
-            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * 1);
-        }
+
         transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
     }
 }
--- Assets/Scripts/AutonomousAgent/Steering.cs
+++ Assets/Scripts/AutonomousAgent/Steering.cs
@@ -24,4 +24,22 @@ public static class Steering
         Vector3 force = CalculateSteering(agent, (agent.transform.position - target.transform.position));
         return force;
     }
+
+    public static Vector3 Wander(AutonomousAgent agent)
+    {
+        // randomly adjust angle +/- displacement 
+        agent.wanderAngle = agent.wanderAngle + Random.Range(-agent.wanderDisplacement, agent.wanderDisplacement);
+        // create rotation quaternion around y-axis (up) 
+        Quaternion rotation = Quaternion.AngleAxis(agent.wanderAngle, Vector3.up);
+        // calculate point on circle radius 
+        Vector3 point = rotation * (Vector3.forward * agent.wanderRadius);
+        // set point in front of agent at distance length 
+        Vector3 forward = agent.transform.forward * agent.wanderDistance;
+
+        Debug.DrawRay(agent.transform.position, forward + point, Color.red);
+
+        Vector3 force = CalculateSteering(agent, forward + point);
+
+        return force;
+    }
 }
