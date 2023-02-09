6c7c119baa1fdc0575976ba888544d765fb6a5f5
wmcentire <wmcentire@student.neumont.edu>
2023-02-08 22:55:07 -0700

    set up for dead state and flee state

---
+++ Assets/Scripts/StateAgent/States/DeadState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.StateAgent.States
+{
+    internal class DeadState : State
+    {
+        public DeadState(global::StateAgent owner) : base(owner)
+        {
+
+        }
+
+        public override void OnEnter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnExit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/StateAgent/States/FleeState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.StateAgent.States
+{
+    internal class FleeState : State
+    {
+        public FleeState(global::StateAgent owner) : base(owner)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnExit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
